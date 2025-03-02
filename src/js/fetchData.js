b95172e0d16cdbd0711e492530e252fa6e9f4878
abdelilah-dev <contactme.abdelilah@gmail.com>
2025-03-02 15:35:10 +0100

    upload the Update

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,5 +1,7 @@
 let personalApiKey = import.meta.env.VITE_API_KEY
 
+
+// get the geoCoding of the target place by city or country name 
 async function getGeocoding(targePlace) {
     document.querySelector(".loading").classList.add("active")
     try {
@@ -7,14 +9,11 @@ async function getGeocoding(targePlace) {
         if (!response.ok) {
             throw new Error(`HTTP error 1! status: ${response.status}`);
         }
-        let data = await response.json();
-        if (data.length === 0) {
+        let Geocoding = await response.json();
+        if (Geocoding.length === 0) {
             document.querySelector(".search-bar .error").innerHTML = "* the country, city is not valid ..."
-        } else {
-            currentWeatherData(data[0].lat, data[0].lon);
-            getWeekWeatherdata(data[0].lat, data[0].lon);
         }
-        return data;
+        return Geocoding;
     } catch (error) {
         throw new Error(`the error 1-2 : ${error}`);
     } finally {
@@ -23,32 +22,47 @@ async function getGeocoding(targePlace) {
 
 }
 
-async function currentWeatherData(lat, lon) {
+// get The current weather of the current day
+async function currentWeatherData(lat, lon, lang) {
     try {
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=ar&appid=${personalApiKey}`);
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=${lang}&appid=${personalApiKey}`);
         if (!response.ok) {
             throw new Error(`HTTP error 2! status: ${response.status}`);
         }
-        let data = await response.json();
-        window.localStorage.setItem("currentWeather", JSON.stringify(data));
-        return data;
+        return await response.json();
     } catch (error) {
         throw new Error(`the error 2-2 : ${error}`);
     }
 }
 
-async function getWeekWeatherdata(lat, lon) {
+// get the week weather
+async function getWeekWeatherdata(lat, lon, lang) {
     try {
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat.toFixed(2)}&lon=${lon.toFixed(2)}&units=metric&lang=ar&appid=${personalApiKey}`);
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat.toFixed(2)}&lon=${lon.toFixed(2)}&units=metric&lang=${lang}&appid=${personalApiKey}`);
         if (!response.ok) {
             throw new Error(`HTTP error 3! status: ${response.status}`);
         }
-        let data = await response.json();
-        window.localStorage.setItem("weekWeather", JSON.stringify(data));
-        return data;
+        return await response.json();
     } catch (error) {
         throw new Error(`the error 3-2 : ${error}`);
     }
 }
 
-export { getGeocoding, currentWeatherData, getWeekWeatherdata }
+// get the translation from en.json or ar.json file and return it 
+async function getTranslateLang(lang) {
+    document.querySelector(".full-loading").classList.add("active")
+    setTimeout(() => {
+        document.querySelector(".full-loading").classList.remove("active")
+    }, 1000);
+    try {
+        let response = await fetch(`/../public/translation/${lang}.json`)
+        if (!response.ok) {
+            throw new Error(`HTTP error 3! status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        throw new Error(`the error 4-2 : ${error}`)
+    }
+}
+
+export { getGeocoding, currentWeatherData, getWeekWeatherdata, getTranslateLang }
--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,107 @@
-import { getGeocoding, currentWeatherData, getWeekWeatherdata } from "./fetchData.js";
+import { getGeocoding, currentWeatherData, getWeekWeatherdata, getTranslateLang } from "./fetchData.js";
 
+
+// let date = new Date("2025-03-02 15:00:00");
+// console.log(date);
+// console.log(date.getDay());
+// console.log(date.toLocaleDateString(`en`, { weekday: 'long' }))
+
+// let mymap = new Map([
+//     ["1", { one: 1 }],
+//     ["1", { one: 1 }],
+//     ["1", { one: 1 }],
+// ])
+// console.log(mymap);
 let input = document.querySelector(".input");
 let SearchBtn = document.querySelector(".search-btn");
+let changLangBtns = document.querySelector(".change-lang");
+let weatherData = {};
+let currentLang = window.localStorage.lang || "en";
 
-window.onload = function () {
-    document.querySelector(".loading").classList.remove("active")
-    if (window.localStorage.currentWeather) {
-        console.log(JSON.parse(window.localStorage.currentWeather));
-        displayCurrentWeather(JSON.parse(window.localStorage.currentWeather))
+
+window.onload = async function () {
+    document.querySelector(".full-loading").classList.remove("active")
+    loadTranslate(currentLang);
+    toggleLanguageClass(currentLang);
+    if (window.localStorage.weatherData) {
+        displayCurrentWeather(JSON.parse(window.localStorage.weatherData).currentWeather, await getTranslateLang(currentLang));
     } else {
-        // give the data from location automatique
-        console.log("give data from location automatique")
+        console.log("no data to show")
     }
 }
 
+// if search btn is pressed
 SearchBtn.onclick = async function (e) {
     e.preventDefault();
     if (input.value !== "") {
         document.querySelector(".search-bar .error").innerHTML = "";
-        let data = await getGeocoding(input.value);
-        if (data.length !== 0) {
-            let currentPlace = await currentWeatherData(data[0].lat, data[0].lon);
-            displayCurrentWeather(currentPlace);
+        try {
+            // wait to fetch data from getgeocoding and return it
+            let geocoding = await getGeocoding(input.value);
+            if (geocoding.length !== 0) {
+                // wait to fetch data from two functions and return it
+                [weatherData.currentWeather, weatherData.weekWeather] = await Promise.all([
+                    currentWeatherData(geocoding[0].lat, geocoding[0].lon, currentLang),
+                    getWeekWeatherdata(geocoding[0].lat, geocoding[0].lon, currentLang)
+                ]);
+                let translate = await getTranslateLang(currentLang);
+                window.localStorage.setItem("weatherData", JSON.stringify(weatherData));
+                displayCurrentWeather(weatherData.currentWeather, translate);
+            }
+        } catch (error) {
+            document.querySelector(".search-bar .error").innerHTML = "* There was an error fetching the weather data.";
+            throw new Error(`The Error : ${error}`)
         }
     }
 }
 
-function displayCurrentWeather(targetPlace) {
+// if select another languge
+changLangBtns.addEventListener("click", async (e) => {
+    let selectLang = e.target.getAttribute("translate-lang");
+    window.localStorage.setItem("lang", selectLang);
+    currentLang = selectLang;
+    displayCurrentWeather(JSON.parse(window.localStorage.weatherData).currentWeather, await getTranslateLang(currentLang))
+    toggleLanguageClass(selectLang);
+    loadTranslate(selectLang);
+})
+
+// display the current weather of today
+function displayCurrentWeather(targetPlace, translations) {
     let weatherDetails = document.querySelector(".current-weather");
     let TodayDetails = document.querySelector(".day-detail");
-    let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
     weatherDetails.innerHTML = `
         <img class="img" src="https://openweathermap.org/img/wn/${targetPlace.weather[0].icon}@2x.png" alt="">
         <div class="temperature">
-            <span>${targetPlace.main.feels_like}<sup>°C</sup></span>
+            <span>${Math.floor(targetPlace.main.feels_like)}<sup>°C</sup></span>
         </div>
         <div class="current-preview">
-            <div class="rain">rain: %</div>
-            <div class="humidity">humidity: ${targetPlace.main.humidity}%</div>
-            <div class="wind">wind: ${Math.floor(targetPlace.wind.speed * 3.6)}km/h</div>
+            <div class="rain">${translations.rain}: %</div>
+            <div class="humidity">${translations.humidity}: ${targetPlace.main.humidity}%</div>
+            <div class="wind">${translations.wind}: ${Math.floor(targetPlace.wind.speed * 3.6)}km/h</div>
         </div>
-    `
+    `;
     let thisDay = new Date();
+    let dayName = thisDay.toLocaleDateString(`${translations.shortLang}`, { weekday: 'long' });
     TodayDetails.innerHTML = `
-        <div class="title">Weather</div>
-        <div class="current-day">${daysOfWeek[thisDay.getDay()]}</div>
+        <div class="title">${translations.weather}</div>
+        <div class="current-day">${dayName}</div>
         <div class="weather-desc">${targetPlace.weather[0].description}</div>
-    `
+    ` ;
+}
+
+// load translate languge from fecthdata file 
+async function loadTranslate(lang) {
+    let translate = await getTranslateLang(lang);
+    applyTranslations(translate);
+}
+// apply the translation on page
+function applyTranslations(translations) {
+    input.placeholder = translations.search_placeholder;
+    SearchBtn.innerHTML = translations.search_button
+    document.querySelector(".slectlang-menu").innerText = translations.lang;
+}
+// if change the lang change the direction of page
+function toggleLanguageClass(selectLang) {
+    document.querySelector(".hero-area").classList.toggle("ltr", selectLang === "en");
+    document.querySelector(".hero-area").classList.toggle("rtl", selectLang !== "en");
 }
